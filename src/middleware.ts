66e993f50d53aad2a1438c7571317a880ce39451
Tigercub12 <tigerg1290@gmail.com>
2026-09-15 21:41:41 +0700

    Revert proxy to middleware for Cloudflare Pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,5 +23,3 @@ export default auth((req) => {
 export const config = {
   matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
 }
-
-export const runtime = 'edge';
