dcad2524cc706749c9758c08a96c4ed4bcd97cfa
Tigercub12 <tigerg1290@gmail.com>
2026-09-15 21:35:14 +0700

    Fix middleware edge runtime config

--- src/proxy.ts
+++ src/proxy.ts
@@ -23,3 +23,5 @@ export default auth((req) => {
 export const config = {
   matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
 }
+
+export const runtime = 'edge';
